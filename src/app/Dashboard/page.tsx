1b6b34dc1c0a53181ec02f66dbb105ad924d7c70
Bhankid <alfredfianyo50@gmail.com>
2025-01-20 04:05:25 +0000

    EarningsStatsCard

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -39,6 +39,7 @@ import {
 } from "recharts";
 import TeacherStatsCard from "../components/TeacherStatsCard";
 import ParentStatsCard from "../components/ParentStatsCard";
+import EarningsStatsCard from "../components/EarningsStatsCard";
 
 type ActiveTab =
   | "dashboard"
@@ -121,20 +122,7 @@ const Dashboard: React.FC = () => {
 
         {/* Earnings Statistics Card */}
         <div className="bg-white p-4 rounded-lg shadow hover:shadow-md transition-shadow">
-          <div className="flex items-center">
-            <div className="bg-red-100 p-2 sm:p-3 rounded-full w-8 sm:w-10 h-8 sm:h-10 flex items-center justify-center relative">
-              <i className="fas fa-dollar-sign text-red-500 text-sm sm:text-base"></i>
-              <div className="absolute left-[40px] sm:left-[50px] top-1/2 transform -translate-y-1/2 h-6 sm:h-8 w-0.5 bg-red-500"></div>
-            </div>
-            <div className="ml-4 sm:ml-6">
-              <p className="text-gray-800 font-medium text-sm sm:text-base">
-                Earnings
-              </p>
-              <p className="text-xl sm:text-2xl font-bold text-gray-900">
-                $200000
-              </p>
-            </div>
-          </div>
+          <EarningsStatsCard />
         </div>
       </div>
 
