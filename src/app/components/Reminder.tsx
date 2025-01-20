9ac10f1dc2e22880db8f7524b61080c6f9551c98
Bhankid <alfredfianyo50@gmail.com>
2025-01-20 20:03:58 +0000

    update 1.1

--- src/app/components/Reminder.tsx
+++ src/app/components/Reminder.tsx
@@ -14,13 +14,13 @@ function Reminder() {
   const [events, setEvents] = useState<EventType[]>([]);
 
   const dateColors = [
-    "bg-teal-400",
-    "bg-yellow-400",
-    "bg-pink-400",
-    "bg-purple-400",
-    "bg-blue-400",
-    "bg-red-400",
-    "bg-green-400",
+    "bg-teal-500",
+    "bg-yellow-500",
+    "bg-pink-500",
+    "bg-purple-500",
+    "bg-blue-500",
+    "bg-red-500",
+    "bg-green-500",
   ];
 
   // Fetch events on component mount
