b05a6de5df18685cd3e5fe6b1c0235a9b7c9b68f
Bhankid <alfredfianyo50@gmail.com>
2025-01-28 22:44:50 +0000

    Added version to the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   // Get the current year dynamically
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-white p-4 text-center shadow-md w-full">
+    <footer className="bg-white p-4 text-center shadow-md w-full flex justify-between">
       <p className="text-gray-600">
-        © Copyrights FredComTech {currentYear}. All rights reserved.
+        &copy; Copyrights <Link href="https://alfred-portfolio-site.onrender.com/" target="_blank" rel="noopener noreferrer" className="text-red-600 hover:text-red-700">FredComTech</Link> {currentYear}. All rights reserved.
+      </p>
+      <p className="text-gray-600 text-sm">
+        v1.0.0-alpha.1
       </p>
     </footer>
   );
